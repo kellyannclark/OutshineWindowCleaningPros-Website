05d48b99c77c61ffc94d1e833741d86a35af1410
kellyannclark <thefaithseed@gmail.com>
2023-12-05 14:53:39 -0700

    changes

--- js/main.js
+++ js/main.js
@@ -1,18 +1,24 @@
 // main.js
 
 import { loadHeaderFooter } from "./utils.mjs";
-loadHeaderFooter();
-
-
-
+import { startPulseAnimation } from './freeEstimateAnimation.mjs';
 import { submitForm } from './quoteForm.mjs';
 
-const submitButton = document.querySelector('#submitBtn');  // Use the id "submitBtn" to select the button
-if (submitButton) {                                         // Check if the button is found before adding the event listener
+// Load header and footer when the page is loaded
+loadHeaderFooter();
+
+// Select the submit button and add an event listener for the click event
+const submitButton = document.querySelector('#submitBtn');
+if (submitButton) {
     submitButton.addEventListener('click', submitForm);
 } else {
     console.error('Submit button not found!');
 }
 
-//This function is listening for the submit button to be clicked
-//When it is clicked, then it passes to the submitForm function w/i the quoteForm.mjs
+// Start the pulse animation directly
+startPulseAnimation();
+
+
+
+
+
